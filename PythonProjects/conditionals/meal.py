a3ed976513f85329d120a2593427b96f921a8ae3
helloimchrisu <79983533+helloimchrisu@users.noreply.github.com>
2024-02-19 19:16:59 +0100

    Update meal.py

--- PythonProjects/conditionals/meal.py
+++ PythonProjects/conditionals/meal.py
@@ -1,17 +1,20 @@
 def main():
     userTime = input("What's the time? ")
-    # hours, minutes = time.split(":")
-    # hours = convert(hours)
-    # minutes = convert(minutes)
     userTime = convert(userTime)
 
+    if 7.00 <= userTime <=8:
+        print("breakfast time!")
+    elif 12 <= userTime <= 13:
+        print("lunch time!")
+    elif 18 <= userTime <= 19:
+        print("dinner time!")
 
-    def convert(time):
-        hours, minutes = time.split(":")
-        hours = float(hours)
-        minutes = float(minutes)
 
-
-
+def convert(time):
+    hours, minutes = time.split(":")
+    hours = float(hours)
+    minutes = float(minutes)    
+    return (hours + (minutes/60)) 
+        
 
 main()
