feec5ef735e066c8ed02c23efbce1c5b8988d6dc
helloimchrisu <79983533+helloimchrisu@users.noreply.github.com>
2024-03-27 20:12:07 +0100

    another not working

---
+++ PythonProjects/loops/nutrition.py
@@ -0,0 +1,22 @@
+
+
+fruits = [
+    {"name" : "apple", "kcal" : "130"},
+    {"name" : "avocado", "kcal" : "50"},
+    {"name" : "banana", "kcal" : "130"},
+    {"name" : "cantaloupe", "kcal" : "50"},
+    {"name" : "grapefruit", "kcal" : "60"},
+    {"name" : "grapes", "kcal" : "90"},
+    {"name" : "honeydew melon", "kcal" : "50"},
+    {"name" : "kiwifruit", "kcal" : "90"},
+    {"name" : "lemon", "kcal" : "15"},
+    {"name" : "lime", "kcal" : "20"},
+    {"name" : "nectarine", "kcal" : "60"}
+]
+
+
+
+user_fruit = input("Item: ")
+if user_fruit in fruits:    
+    print(fruits[user_fruit])
+        
--- PythonProjects/loops/test.py
+++
@@ -1,8 +0,0 @@
-plate = input()
-array_lenght = len(plate) - 1
-for s in plate:
-    if s.isnumeric() == True and plate[array_lenght].isalpha() == True:
-        print("true")
-    else:
-        print("false")
-   
