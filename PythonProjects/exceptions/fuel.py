484265c4dedcec115984cb80a3179a7081e7ee97
helloimchrisu <79983533+helloimchrisu@users.noreply.github.com>
2024-04-16 19:29:35 +0200

    its working!
    added try / except block and its working!

--- PythonProjects/exceptions/fuel.py
+++ PythonProjects/exceptions/fuel.py
@@ -1,5 +1,17 @@
-user_fraction = input("Fraction:")
-x, y = user_fraction.split("/")
-fraction = (int(x)/int(y)) * 100
-rounded_fraction = round(fraction)
-print(f"{rounded_fraction}%")
+while True:
+    try:
+            user_fraction = input("Fraction:")
+            x, y = user_fraction.split("/")
+            fraction = (int(x)/int(y)) * 100
+            rounded_fraction = round(fraction)        
+            if fraction >= 99:
+                print("F")
+            elif fraction <= 1:
+                print("E")
+            else:
+                print(f"{rounded_fraction}%")
+            break
+    except ValueError:
+        pass
+    except ZeroDivisionError:
+         pass
